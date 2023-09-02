47171763eb2fe64c3ad44850881bf6fcf018c39c
m4ki3e <kz3ebr@gmail.com>
2023-09-02 21:25:26 +0800

    update config file

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
 const RSAUtils = require('./utils/security');
-const config = require('./config.json')
+const configPath = path.join( process.cwd(), 'config.json')
 
 async function main() {
-
     console.log(formatData(new Date()))
-
-    let userId = config.username
-    let password = config.password
-    let service = encodeURIComponent(encodeURIComponent(config.service))
-
-    //get pageInfo
-    axios.get('http://10.8.2.2').then(res => {
-        // logger.log(res.data)
-        let s1 = res.data
-        let s2 = s1.slice(s1.indexOf('wlanuserip'), s1.indexOf('\'</script>'))
-        let macString = s2.slice(s2.indexOf('&mac=') + 5, s2.indexOf('&t=wireless'))
-        let queryString = encodeURIComponent(encodeURIComponent(s2))
-        //get RSAInfo
-        let infoUrl = 'http://10.8.2.2/eportal/InterFace.do?method=pageInfo'
-        let infoData = 'queryString=' + queryString
-        axios.post(infoUrl, infoData).then(res => {
+    fs.readFile(configPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const config = JSON.parse(data);
+        let userId =  config.userId
+        let password =  config.password
+        let service = encodeURIComponent(encodeURIComponent(config.service))
+        //get pageInfo
+        axios.get('http://10.8.2.2').then(res => {
             // logger.log(res.data)
-            let publicKeyExponent = res.data.publicKeyExponent
-            let publicKeyModulus = res.data.publicKeyModulus
-            //encrypt pw
-            let passwordMac = password + ">" + macString
-            let passwordEncode = passwordMac.split("").reverse().join("")
-            RSAUtils.setMaxDigits(400)
-            let key = new RSAUtils.getKeyPair(publicKeyExponent, "", publicKeyModulus)
-            let newPw = RSAUtils.encryptedString(key, passwordEncode)
-            newPw = encodeURIComponent(encodeURIComponent(newPw))
-            //login
-            let loginUrl = 'http://10.8.2.2/eportal/InterFace.do?method=login'
-            let loginData = "userId=" + userId + "&password=" + newPw +
-                "&service=" + service + "&queryString=" + queryString +
-                "&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=true"
-            axios.post(loginUrl, loginData).then(res => {
-                console.log(res.data)
+            let s1 = res.data
+            let s2 = s1.slice(s1.indexOf('wlanuserip'), s1.indexOf('\'</script>'))
+            let macString = s2.slice(s2.indexOf('&mac=') + 5, s2.indexOf('&t=wireless'))
+            let queryString = encodeURIComponent(encodeURIComponent(s2))
+            //get RSAInfo
+            let infoUrl = 'http://10.8.2.2/eportal/InterFace.do?method=pageInfo'
+            let infoData = 'queryString=' + queryString
+            axios.post(infoUrl, infoData).then(res => {
+                // logger.log(res.data)
+                let publicKeyExponent = res.data.publicKeyExponent
+                let publicKeyModulus = res.data.publicKeyModulus
+                //encrypt pw
+                let passwordMac = password + ">" + macString
+                let passwordEncode = passwordMac.split("").reverse().join("")
+                RSAUtils.setMaxDigits(400)
+                let key = new RSAUtils.getKeyPair(publicKeyExponent, "", publicKeyModulus)
+                let newPw = RSAUtils.encryptedString(key, passwordEncode)
+                newPw = encodeURIComponent(encodeURIComponent(newPw))
+                //login
+                let loginUrl = 'http://10.8.2.2/eportal/InterFace.do?method=login'
+                let loginData = "userId=" + userId + "&password=" + newPw +
+                    "&service=" + service + "&queryString=" + queryString +
+                    "&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=true"
+                axios.post(loginUrl, loginData).then(res => {
+                    console.log(res.data)
+                })
             })
         })
-    })
+    });
+
 }
 
 function formatData(timestamp) {
