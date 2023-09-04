577b2f4183586f531be5c4d72aad032f6d443ec8
m4ki3e <kz3ebr@gmail.com>
2023-09-04 09:40:10 +0800

    update log info

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const RSAUtils = require('./utils/security');
 const configPath = path.join( process.cwd(), 'config.json')
 
 async function main() {
-    console.log(formatData(new Date()))
+    const date = new Date();
+    const options = { timeZone: 'Asia/Shanghai' };
+    const formattedDate = date.toLocaleString('en-US', options);
+    console.log(formattedDate)
     fs.readFile(configPath, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
@@ -51,17 +54,6 @@ async function main() {
         })
     });
 
-}
-
-function formatData(timestamp) {
-    const date = new Date(timestamp);
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
-    const hour = date.getHours().toString().padStart(2, '0');
-    const minute = date.getMinutes().toString().padStart(2, '0');
-    const second = date.getSeconds().toString().padStart(2, '0');
-    return `${year}-${month}-${day}T${hour}-${minute}-${second}`
 }
 
 main()
